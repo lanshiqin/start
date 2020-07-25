919079f9ba74e8b3e64814ca706b3cf0c35211e8
lanshiqin <lanshiqin@outlook.com>
2020-07-26 01:04:40 +0800

    init start project

---
+++ boot/src/main/java/com/lanshiqin/start/StartApplication.java
@@ -0,0 +1,10 @@
+package com.lanshiqin.start;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
