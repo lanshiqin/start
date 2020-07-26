afa387575f95287984cb4af6fe801a849d04a429
lanshiqin <lanshiqin@outlook.com>
2020-07-26 09:39:13 +0800

    add Spring Boot to boot Project module

--- boot/src/main/java/com/lanshiqin/start/StartApplication.java
+++ boot/src/main/java/com/lanshiqin/start/StartApplication.java
@@ -2,6 +2,10 @@ package com.lanshiqin.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动应用
+ * @author 蓝士钦
+ */
 @SpringBootApplication
 public class StartApplication {
     public static void main(final String[] args) {
---
+++ boot/src/test/java/com/lanshiqin/start/StartApplicationTest.java
@@ -0,0 +1,16 @@
+package com.lanshiqin.start;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+@SpringBootTest
+public class StartApplicationTest extends AbstractTestNGSpringContextTests {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
